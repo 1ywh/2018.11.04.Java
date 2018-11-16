9c18eec0c70de09e0382c41c49efd77a8a914e5b
1ywh <3356268629@qq.com>
2018-11-16 23:46:37 +0800

    补充

--- NoName.java
+++ NoName.java
@@ -12,17 +12,21 @@ interface MyInterface{
 	
 }
 class Outer{
+	
 	private int num=9;
 	//final int value 调用以后不再修改
 	
 	public void display(int value){
 	    
 		//匿名内部类实现了一个接口
-		new  MyInterface(){		
-		  
+		//匿名内部类没办法创建更多的对象只能有一个
+		new  MyInterface(){				  
 		  public void test(){
-			   System.out.println(num++);
-			   System.out.println(value);
+			   System.out.println(num);//9
+			   System.out.println(num++);//9
+			   System.out.println(value);//20
+			   //System.out.println(value++);//error,不能修改
+			   
 		}		
 	}.test();
   }	  
